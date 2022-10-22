edd2513ed96cb3169a202b0e1fe02d20edd93f71
Xiexieni9527 <62736559+Xiexieni9527@users.noreply.github.com>
2022-10-22 22:32:20 +0800

    initialize

--- AT/automation/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Document(models.Model):
-    document=models.FileField(upload_to='uploads/')
-    create_at=models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering=('-create_at',)
--- AT/automation/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path,include
-from automation import views
-
-urlpatterns=[
-    path('uploads/',views.upload_file.as_view(),name='uploads/')
-]
